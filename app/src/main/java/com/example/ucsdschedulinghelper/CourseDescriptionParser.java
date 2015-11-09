cded75622611ae78b2654e18377ef36b8bb29611
beckychow <beckydzau@gmail.com>
2015-11-09 01:46:36 -0800

    Change in Parser
    Clear hyper link in course description and move them to async

--- app/src/main/java/com/example/ucsdschedulinghelper/CourseDescriptionParser.java
+++ app/src/main/java/com/example/ucsdschedulinghelper/CourseDescriptionParser.java
@@ -6,6 +6,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
@@ -41,91 +42,105 @@ public class CourseDescriptionParser extends MyHtmlParser {
     }
 
     void parseContentToDB() {
-
-        // Tags for parsing
-        final String DIVISION_TAG = "<h2 class=\"course-subhead-1\">";
-        final String ID_TAG = "<a id=\"";
-        final String NAME_TAG = "<p class=\"course-name\">";
-        final String DESCRIPTION_TAG = "<p class=\"course-descriptions\">";
-
-        // Lower division, Upper division, Graduate
-        int index = content.indexOf(DIVISION_TAG);
-        final String[] division = {"Lower", "Upper", "Graduate"};
-        // int divi = 0;
-        while (index != -1) {
-            int next_idx = content.indexOf(DIVISION_TAG, index + DIVISION_TAG.length());
-            // get description for each class in one division
-            while (index < next_idx) {
-                int tmp_idx;
-                String id, fullCode, title, description, units;
-                // int unit;
-
-                // get course id
-                index = content.indexOf(ID_TAG, index) + ID_TAG.length();
-                tmp_idx = content.indexOf('"', index);
-                id = content.substring(index, tmp_idx);
-
-                // get course fullCode
-                index = content.indexOf(NAME_TAG, index) + NAME_TAG.length();
-                tmp_idx = content.indexOf('.', index);
-                fullCode = content.substring(index, tmp_idx);
-                fullCode = removeExtraSpace(fullCode);
-
-                // get course title
-                index = tmp_idx + 2;
-                tmp_idx = content.indexOf('(', index);
-                title = content.substring(index,tmp_idx - 1);
-                title = removeExtraSpace(title);
-
-                // get course units
-                // TODO: resolve issue of varied unit in same course (using String now)
-                index = tmp_idx + 1;
-                tmp_idx = content.indexOf(')', index);
-                units = content.substring(index, tmp_idx);
-                /** unit = Integer.parseInt(content.substring(index, tmp_idx)); */
-
-                // get course description
-                index = content.indexOf(DESCRIPTION_TAG, index) + DESCRIPTION_TAG.length();
-                tmp_idx = content.indexOf("<strong", index) - 1;
-                description = content.substring(index, tmp_idx);
-                description = removeExtraSpace(description);
-                // TODO: get prerequisites
-                // TODO: division category
-                // test
-
-                /** Additions by SKE **/
-                int indexOfTheDivider = fullCode.indexOf(" ");
-                String department = fullCode.substring(0, indexOfTheDivider);
-                String code = fullCode.substring(indexOfTheDivider + 1);
-
-                changeText(id, department, code, title, description, units);
-                addToDatabase(id, department, code, title, description, units, false);
-            }
-            index = next_idx;
-        }
+        new parseContentToDBTask().execute(fetchDataFromHttp);
     }
 
-    private String removeExtraSpace(String s) {
-        boolean space = false;
-        char[] charArray = new char[s.length()];
-        int len = 0;
-        for (int i = 0; i < s.length(); ++i)
-        {
-            int tmp = s.charAt(i);
-            if (s.charAt(i) <= ' ' ||
-                    s.charAt(i) > '~')
-            {
-                if (space)
-                    continue;
-                space = true;
-                charArray[len++] = ' ';
-            } else
-            {
-                space = false;
-                charArray[len++] = s.charAt(i);
-            }
+    private class parseContentToDBTask extends parseTask {
+        @Override
+        public void onPreExecute() {
+
         }
-        return String.valueOf(charArray, 0, len);
+
+        @Override
+        public Void doInBackground(FetchDataFromHttp... params) {
+            // get text format of the html file
+            String content = params[0].getResults();
+
+            // Tags for parsing
+            final String DIVISION_TAG = "<h2 class=\"course-subhead-1\">";
+            final String ID_TAG = "<a id=\"";
+            final String NAME_TAG = "<p class=\"course-name\">";
+            final String DESCRIPTION_TAG = "<p class=\"course-descriptions\">";
+
+            // Lower division, Upper division, Graduate
+            int index = content.indexOf(DIVISION_TAG);
+            final String[] division = {"Lower", "Upper", "Graduate"};
+            // int divi = 0;
+            while (index != -1) {
+                int next_idx = content.indexOf(DIVISION_TAG, index + DIVISION_TAG.length());
+                // get description for each class in one division
+                while (index < next_idx) {
+                    int tmp_idx;
+                    String id, fullCode, title, description, units;
+
+                    // get course id
+                    index = content.indexOf(ID_TAG, index) + ID_TAG.length();
+                    tmp_idx = content.indexOf('"', index);
+                    id = content.substring(index, tmp_idx);
+
+                    // get course fullCode
+                    index = content.indexOf(NAME_TAG, index) + NAME_TAG.length();
+                    tmp_idx = content.indexOf('.', index);
+                    fullCode = content.substring(index, tmp_idx);
+                    fullCode = removeExtraSpaces(fullCode);
+
+                    // get course title
+                    index = tmp_idx + 2;
+                    tmp_idx = content.indexOf('(', index);
+                    title = content.substring(index, tmp_idx - 1);
+                    title = removeExtraSpaces(title);
+
+                    // get course units
+                    index = tmp_idx + 1;
+                    tmp_idx = content.indexOf(')', index);
+                    units = content.substring(index, tmp_idx);
+                    /** unit = Integer.parseInt(content.substring(index, tmp_idx)); */
+
+                    // get course description
+                    index = content.indexOf(DESCRIPTION_TAG, index) + DESCRIPTION_TAG.length();
+                    tmp_idx = content.indexOf("<strong", index) - 1;
+                    description = content.substring(index, tmp_idx);
+                    description = removeInsideTags(description);
+                    description = removeExtraSpaces(description);
+                    // TODO: get prerequisites
+                    // TODO: division category
+
+                    /** Additions by SKE **/
+                    int indexOfTheDivider = fullCode.indexOf(" ");
+                    String department = fullCode.substring(0, indexOfTheDivider);
+                    String code = fullCode.substring(indexOfTheDivider + 1);
+
+                    //changeText(id, department, code, title, description, units);
+                    addToDatabase(id, department, code, title, description, units, false);
+                }
+                index = next_idx;
+            }
+            return null;
+        }
+
+        private void addToDatabase(String id, String department, String code, String title,
+                                   String description, String units, boolean update) {
+
+            // Create a new map of values, where column names are the keys
+            ContentValues values = new ContentValues();
+            values.put(Course.COLUMN_ENTRY_ID, id);
+            values.put(Course.COLUMN_DEPARTMENT, department);
+            values.put(Course.COLUMN_CODE, code);
+            values.put(Course.COLUMN_TITLE, title);
+            values.put(Course.COLUMN_DESCRIPTION, description);
+            values.put(Course.COLUMN_UNITS, units);
+
+            // Insert the new row, returning the primary key value of the new row
+            // long newRowId = db.insert(Course.TABLE_NAME, "null", values);
+        /*if (once) {
+            long newRowId = db.insert(Course.TABLE_NAME, "null", values);
+            once = false;
+        }*/
+            String selection = "entryid LIKE ?";
+            String[] selectionArgs = {id};
+            contentResolver.update(CoursesContentProvider.CONTENT_URI, values,
+                    selection, selectionArgs);
+        }
     }
 
 
@@ -144,28 +159,5 @@ public class CourseDescriptionParser extends MyHtmlParser {
         textView.setText(newString);
     }
 
-    private void addToDatabase(String id, String department, String code, String title,
-                               String description, String units, boolean update) {
 
-        // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(Course.COLUMN_ENTRY_ID, id);
-        values.put(Course.COLUMN_DEPARTMENT, department);
-        values.put(Course.COLUMN_CODE, code);
-        values.put(Course.COLUMN_TITLE, title);
-        values.put(Course.COLUMN_DESCRIPTION, description);
-        values.put(Course.COLUMN_UNITS, units);
-
-        // Insert the new row, returning the primary key value of the new row
-        // long newRowId = db.insert(Course.TABLE_NAME, "null", values);
-        /*if (once) {
-            long newRowId = db.insert(Course.TABLE_NAME, "null", values);
-            once = false;
-        }*/
-        String selection = "entryid LIKE ?";
-        String[] selectionArgs = { id };
-        contentResolver.update(CoursesContentProvider.CONTENT_URI, values,
-                selection, selectionArgs);
-
-    }
 }
--- app/src/main/java/com/example/ucsdschedulinghelper/MyHtmlParser.java
+++ app/src/main/java/com/example/ucsdschedulinghelper/MyHtmlParser.java
@@ -1,6 +1,7 @@
 package com.example.ucsdschedulinghelper;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.util.Log;
 
 
@@ -9,11 +10,9 @@ import android.util.Log;
  * Abstract class MyHtmlParser is to parse html url of course description, information or cape.
  */
 
-// TODO: use Async Task for this class
-abstract public class MyHtmlParser {
+abstract public class MyHtmlParser{
     String urlToParse;
     Activity myActivity;
-    String content;
     FetchDataFromHttp fetchDataFromHttp;
 
     public MyHtmlParser(Activity activity, String url) {
@@ -21,10 +20,52 @@ abstract public class MyHtmlParser {
         myActivity = activity;
         try {
             fetchDataFromHttp = new FetchDataFromHttp(myActivity.getApplicationContext(), urlToParse);
-            content = fetchDataFromHttp.getResults();
+            //content = fetchDataFromHttp.getResults();
         } catch (Exception e)
         {
             Log.e("CourseDescriptionParser", "parseContentToDB() " + e);
+        }
+    }
+
+    abstract protected class parseTask extends AsyncTask<FetchDataFromHttp, Void, Void>
+    {
+        @Override
+        abstract public Void doInBackground(FetchDataFromHttp... params);
+
+        protected String removeExtraSpaces(String s) {
+            boolean space = false;
+            char[] charArray = new char[s.length()];
+            int len = 0;
+            for (int i = 0; i < s.length(); ++i)
+            {
+                if (s.charAt(i) <= ' ' ||
+                        s.charAt(i) > '~')
+                {
+                    if (space)
+                        continue;
+                    space = true;
+                    charArray[len++] = ' ';
+                } else
+                {
+                    space = false;
+                    charArray[len++] = s.charAt(i);
+                }
+            }
+            return String.valueOf(charArray, 0, len);
+        }
+
+        protected String removeInsideTags(String s) {
+            char[] charArray = new char[s.length()];
+            int len = 0;
+            boolean insideTag = false;
+            for (int i = 0; i < s.length(); ++i) {
+                if (s.charAt(i) == '<') insideTag = true;
+                else if (s.charAt(i) == '>') insideTag = false;
+                else if (!insideTag) {
+                    charArray[len++] = s.charAt(i);
+                }
+            }
+            return String.valueOf(charArray, 0, len);
         }
     }
 
