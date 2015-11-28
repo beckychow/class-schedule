adf76088a99f6e898b6b84bbe61a93bfceb799e7
Freddy <freddysauceda@gmail.com>
2015-11-28 14:53:16 -0800

    The four year plan now adds the classes that have been parsed from the database (for the most part pending some changes to the database).

--- app/src/main/java/com/example/ucsdschedulinghelper/ui/fourYearPlan/fypData.java
+++ app/src/main/java/com/example/ucsdschedulinghelper/ui/fourYearPlan/fypData.java
@@ -1,7 +1,13 @@
 package com.example.ucsdschedulinghelper.ui.fourYearPlan;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.example.ucsdschedulinghelper.provider.DbContentProvider;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -9,51 +15,130 @@ import java.util.List;
  */
 public class fypData {
 
-    public static HashMap<String, List<String>> getInfo(){
-        HashMap<String, List<String>> classDetails = new HashMap<String, List<String>>();
+    public static LinkedHashMap<String, List<String>> getInfo(Context c){
+        LinkedHashMap<String, List<String>> classDetails = new LinkedHashMap<String, List<String>>();
         //Add the classes you need for the four year plan for each quarter. As of now add some test
         //data
+
+
+        Cursor cr = c.getContentResolver().query(Uri.withAppendedPath(DbContentProvider.CONTENT_PLAN_URI,
+                DbContentProvider.ALL_PATH_AUX), null, null, null, null);
+
+
         List<String> fall_classes = new ArrayList<String>();
-        fall_classes.add("Test");
+        //Iterate over the cursor
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+           if(cr.getString(2).equals("1") && cr.getString(3).equals("1")){
+                fall_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> winter_classes = new ArrayList<String>();
-        winter_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("1") && cr.getString(3).equals("2")){
+                winter_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> spring_classes = new ArrayList<String>();
-        spring_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("1") && cr.getString(3).equals("3")){
+                spring_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> fall2_classes = new ArrayList<String>();
-        fall2_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("2") && cr.getString(3).equals("1")){
+                fall2_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> winter2_classes = new ArrayList<String>();
-        winter2_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("2") && cr.getString(3).equals("2")){
+                winter2_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> spring2_classes = new ArrayList<String>();
-        spring2_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("2") && cr.getString(3).equals("3")){
+                spring2_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> fall3_classes = new ArrayList<String>();
-        fall3_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("3") && cr.getString(3).equals("1")){
+                fall3_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> winter3_classes = new ArrayList<String>();
-        winter3_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("3") && cr.getString(3).equals("2")){
+                winter3_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> spring3_classes = new ArrayList<String>();
-        spring3_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("3") && cr.getString(3).equals("3")){
+                spring3_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> fall4_classes = new ArrayList<String>();
-        fall4_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("4") && cr.getString(3).equals("1")){
+                fall4_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> winter4_classes = new ArrayList<String>();
-        winter4_classes.add("Test");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("4") && cr.getString(3).equals("2")){
+                winter4_classes.add(cr.getString(1));
+            }
+        }
+        cr.moveToFirst();
 
         List<String> spring4_classes = new ArrayList<String>();
-        spring4_classes.add("Test_1");
-        spring4_classes.add("Test_2");
+        while(cr.moveToNext()){
+            //If you should take this class in your first year,first quarter then add to the plan
+            if(cr.getString(2).equals("4") && cr.getString(3).equals("3")){
+                spring4_classes.add(cr.getString(1));
+            }
+        }
+        cr.close();
 
         //Put everything in the hashmap, shouldn't need to mess with this later
         classDetails.put("Freshman Fall", fall_classes);
         classDetails.put("Freshman Winter", winter_classes);
         classDetails.put("Freshman Spring", spring_classes);
+
 
         classDetails.put("Sophomore Fall", fall2_classes);
         classDetails.put("Sophomore Winter", winter2_classes);
@@ -66,6 +151,7 @@ public class fypData {
         classDetails.put("Senior Fall", fall4_classes);
         classDetails.put("Senior Winter", winter4_classes);
         classDetails.put("Senior Spring", spring4_classes);
+
 
         return classDetails;
     }
--- app/src/main/java/com/example/ucsdschedulinghelper/ui/fourYearPlan/fypView.java
+++ app/src/main/java/com/example/ucsdschedulinghelper/ui/fourYearPlan/fypView.java
@@ -1,6 +1,7 @@
 package com.example.ucsdschedulinghelper.ui.fourYearPlan;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
 
@@ -8,23 +9,25 @@ import com.example.ucsdschedulinghelper.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
  * Created by Freddy on 11/14/2015.
  */
 public class fypView extends Activity {
-    HashMap<String, List<String>> quarters;
+    LinkedHashMap<String, List<String>> quarters;
     List<String> classes;
     ExpandableListView exp_list;
     fypAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.four_year_plan);
         exp_list = (ExpandableListView) findViewById(R.id.expandableListView);
-        quarters = fypData.getInfo();
+        quarters = fypData.getInfo(getApplicationContext());
         classes = new ArrayList<String>(quarters.keySet());
         adapter = new fypAdapter(this, quarters, classes);
         exp_list.setAdapter(adapter);
