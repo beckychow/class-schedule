76f79a92054fe184722877142241479497b87b33
beckychow <beckydzau@gmail.com>
2015-11-29 23:47:32 -0800

    updated: four year plan as main page after first launch

--- app/src/main/java/com/example/ucsdschedulinghelper/ui/Splash.java
+++ app/src/main/java/com/example/ucsdschedulinghelper/ui/Splash.java
@@ -10,6 +10,7 @@ import android.os.Handler;
 
 import com.example.ucsdschedulinghelper.MajorChoiceActivity;
 import com.example.ucsdschedulinghelper.R;
+import com.example.ucsdschedulinghelper.ui.fourYearPlan.fypView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -17,6 +18,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class Splash extends Activity {
 
@@ -38,8 +40,21 @@ public class Splash extends Activity {
             @Override
             public void run() {
                 // TODO Auto-generated method stub
-                Intent i = new Intent(Splash.this, MajorChoiceActivity.class);
-                startActivity(i);
+                /** added by Huayin Zhou */
+                // first launch
+                Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).
+                        getBoolean("isFirstRun", true);
+                if (isFirstRun) {
+                    Intent intent = new Intent(Splash.this, MajorChoiceActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(Splash.this, "First Run", Toast.LENGTH_LONG).show();
+                    getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                            .putBoolean("isFirstRun", false).commit();
+                } else {
+                    Intent i = new Intent(Splash.this, fypView.class);
+                    startActivity(i);
+                }
+
                 this.finish();
             }
 
